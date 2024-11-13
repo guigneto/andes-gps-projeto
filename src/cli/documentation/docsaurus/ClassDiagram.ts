5afe76037bd0a31a8f1177bfba687b423f8aeba1
ArthurCremasco <arthurcremascoamaral7@gmail.com>
2024-11-13 16:11:11 -0300

    Fixing Version

--- src/cli/documentation/docsaurus/ClassDiagram.ts
+++ src/cli/documentation/docsaurus/ClassDiagram.ts
@@ -69,7 +69,6 @@ sidebar_position: 4
 ${useCases.map(usecase=> this.createUseCaseContain(usecase)).join('\n')}
 `      
 fs.writeFileSync(path.join(this.targetFolder , `modeloestrutural.md`), value)
-
 }
     private createUseCaseContain(modulo: Module):string {
         return expandToStringWithNL`
